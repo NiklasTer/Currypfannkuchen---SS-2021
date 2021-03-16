5c1b70defbf51dfd12570cdbdd2bae78024bed05
aantooniiaa <aantooniiaa@users.noreply.github.com>
2021-03-16 15:10:48 +0100

    neuer Projektordner
    Beispiel taucht nicht auf Website auf

--- public/0_currypfannkuchen_projekt/js/test.js
+++ public/0_currypfannkuchen_projekt/js/test.js
@@ -4,6 +4,7 @@ socket.on('connected', function (msg) {
     console.log(msg);
 });
 
+
 // Sending a userID will help to know if the message came from me or from others
 let myUserID = Math.random().toString(36).substr(2, 9).toUpperCase();
 
@@ -11,10 +12,10 @@ let myUserID = Math.random().toString(36).substr(2, 9).toUpperCase();
 // Your script starts here ------------------------------------------------------
 
 
-    // Sending an event 
-    socket.emit('serverEvent', myUserID, bgColor);
-}
+ // Sending an event 
+ socket.emit('serverEvent', myUserID, bgColor);
 
+ 
 // Incoming events 
 socket.on('serverEvent', function (user, message) {
     console.log("Incoming event: ", user, message);
