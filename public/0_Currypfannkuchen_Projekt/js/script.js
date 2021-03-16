da7d4eec542b661d6821de15011b60f7c76821f1
aantooniiaa <aantooniiaa@users.noreply.github.com>
2021-03-16 14:38:35 +0100

    neuer Projektordner
    "Currypfannkuchen Projekt". Leere Unterordner, zum neu anfangen

---
+++ public/0_Currypfannkuchen_Projekt/js/script.js
@@ -0,0 +1,22 @@
+// Connecting to server. Don't touch this :-) 
+let socket = io();
+socket.on('connected', function (msg) {
+    console.log(msg);
+});
+
+// Sending a userID will help to know if the message came from me or from others
+let myUserID = Math.random().toString(36).substr(2, 9).toUpperCase();
+
+
+// Your script starts here ------------------------------------------------------
+
+
+    // Sending an event 
+    socket.emit('serverEvent', myUserID, bgColor);
+}
+
+// Incoming events 
+socket.on('serverEvent', function (user, message) {
+    console.log("Incoming event: ", user, message);
+    body.style.backgroundColor = message;
+});
